bb05d0d653125a25877a98fe73c248260979ea61
sharat789 <sharat157@gmail.com>
2023-03-13 20:45:25 +0200

    2

--- src/fxControllers/MainPage.java
+++ src/fxControllers/MainPage.java
@@ -7,16 +7,15 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import model.Truck;
-import model.TyreType;
-import model.User;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.TilePane;
+import model.*;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MainPage implements Initializable {
@@ -35,26 +34,33 @@ public class MainPage implements Initializable {
 
     @FXML
     public ListView<Truck> truckListField;
+    public ListView cargoListField;
+    public TextField titleField;
+    public TextField weightField;
+    public ComboBox CargoTypeField;
+    public TextField customerField;
+    public TextArea descriptionField;
 //    Observable<Truck> items = FXCollections.observableArrayList();
 
     private User loggedUser;
 
 
-//    private List<Truck> truckList = new ArrayList<>();
-@Override
-public void initialize(URL location, ResourceBundle resources) {
+    //    private List<Truck> truckList = new ArrayList<>();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
-}
+    }
 
 
-    public void setInfo(User user){
+    public void setInfo(User user) {
         this.loggedUser = user;
     }
-    public MainPage(){
+
+    public void changeFields(MouseEvent mouseEvent) {
         truckListField.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Truck>() {
             @Override
             public void changed(ObservableValue<? extends Truck> observable, Truck oldValue, Truck newValue) {
-                if(newValue != null){
+                if (newValue != null) {
                     makeField.setText(newValue.getMake());
                     modelField.setText(newValue.getModel());
                     yearField.setText(String.valueOf(newValue.getYear()));
@@ -67,9 +73,10 @@ public void initialize(URL location, ResourceBundle resources) {
         });
     }
 
+
     public void createTruck() {
 //        System.out.println(loggedUser);
-        Truck truck = new Truck(makeField.getText(), modelField.getText(),Integer.parseInt(yearField.getText()), Double.parseDouble(odometerField.getText()), Double.parseDouble(fuelCapacityField.getText()),  TyreType.valueOf(String.valueOf(tyreTypeField.getSelectionModel().getSelectedItem())));
+        Truck truck = new Truck(makeField.getText(), modelField.getText(), Integer.parseInt(yearField.getText()), Double.parseDouble(odometerField.getText()), Double.parseDouble(fuelCapacityField.getText()), TyreType.valueOf(String.valueOf(tyreTypeField.getSelectionModel().getSelectedItem())));
 //        truckList.add(truck);
         truckListField.getItems().add(truck);
 //        if(truckList!= null) {
@@ -93,7 +100,7 @@ public void initialize(URL location, ResourceBundle resources) {
         selectedTruck.setFuelTankCapacity(Double.parseDouble(fuelCapacityField.getText()));
         selectedTruck.setTyreType(TyreType.valueOf(String.valueOf(tyreTypeField.getSelectionModel().getSelectedItem())));
         System.out.println(index);
-        truckListField.getItems().set(index,selectedTruck);
+        truckListField.getItems().set(index, selectedTruck);
 //        truckList.set(index, selectedTruck);
 //        if(truckList!= null) {
 //            for (Truck truck1 : truckList) {
@@ -104,6 +111,28 @@ public void initialize(URL location, ResourceBundle resources) {
     }
 
     public void deleteTruck() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Delete Item");
+        alert.setContentText("Are you sure you want to delete this item?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            Truck selectedTruck = truckListField.getSelectionModel().getSelectedItem();
+            truckListField.getItems().remove(selectedTruck);
+        }
+
+
+    }
+
+    public void createCargo() {
+        Cargo cargo = new Cargo(titleField.getText(), Double.parseDouble(weightField.getText()), CargoType.valueOf(String.valueOf(CargoTypeField.getSelectionModel().getSelectedItem())), descriptionField.getText(), customerField.getText());
+    }
+
+    public void deleteCargo() {
+    }
+
+    public void updateCargo() {
     }
 
 
